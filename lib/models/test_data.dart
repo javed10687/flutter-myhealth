92e60a36f8c0bcdddcc940b1d2094dfacb5847f5
Kunal Khoche <kunal4khoche@gmail.com>
2020-03-24 00:44:39 +0530

    Add files via upload

--- lib/models/test_data.dart
+++ lib/models/test_data.dart
@@ -1,20 +1,26 @@
-List labs = [
-  {
-    "name": "Thyrocare",
-    "logo_img": "https://www.thyrocare.com/images/Thyrocare-Logo.png"
-  },
-  {
-    "name": "Dr Lal",
-    "logo_img":
-        "https://d2qhgd0topi90o.cloudfront.net/website/images/lalpath-labs-logo.png"
-  },
-  {
-    "name": "PathCare",
-    "logo_img":
-        "http://www.pathcarelabs.com/wp-content/uploads/2019/09/cropped-cropped-path-diagnostics-high.gif"
-  },
-  {
-    "name": "RML",
-    "logo_img": "http://www.rmlpathology.com/theme/web/core/rml/images/logo.png"
-  }
-];
+class LabsDetails{
+  final String name;
+  final String logo_img;
+  LabsDetails(this.name, this.logo_img);
+}
+
+  List labs = [
+    {
+      "name": "Thyrocare",
+      "logo_img": "https://www.thyrocare.com/images/Thyrocare-Logo.png"
+    },
+    {
+      "name": "Dr Lal",
+      "logo_img":
+      "https://d2qhgd0topi90o.cloudfront.net/website/images/lalpath-labs-logo.png"
+    },
+    {
+      "name": "PathCare",
+      "logo_img":
+      "http://www.pathcarelabs.com/wp-content/uploads/2019/09/cropped-cropped-path-diagnostics-high.gif"
+    },
+    {
+      "name": "RML",
+      "logo_img": "http://www.rmlpathology.com/theme/web/core/rml/images/logo.png"
+    }
+  ];
